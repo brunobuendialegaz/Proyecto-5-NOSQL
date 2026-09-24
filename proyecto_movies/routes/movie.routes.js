bad760a0c7c70f96bbb06df720628f1e02681a40
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2026-09-24 12:39:17 +0200

    ej.4 metodo delete

--- proyecto_movies/routes/movie.routes.js
+++ proyecto_movies/routes/movie.routes.js
@@ -91,4 +91,16 @@ router.put("/edit/:id", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await Movie.findByIdAndDelete(id);
+
+    return res.status(200).json("Movie deleted!");
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 module.exports = router;
